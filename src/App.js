f7fbc77df836ebc528fbaf5f7023ccf426d63673
bnbanza <bnbanza@gmail.com>
2020-05-25 06:28:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, {useState, useEffect} from 'react';
+import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
+
+function App() {
+
+  //state de la app
+  const [busqueda, guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
+  const [paginaactual, guardarPaginaActual] =useState(1);
+  const [totalpaginas, guardarTotalPaginas] = useState(5);
+
+  useEffect(() => {
+   
+    const consultarAPI = async () => {
+      if(busqueda === '') return;
+
+      const imagenesPorPagina = 30;
+      const key = '14055589-4334771465ce2cb12b9815028';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
+  
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+
+      //calcular total de paginas
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
+      guardarTotalPaginas(calcularTotalPaginas);
+
+      //Mover la pantalla hacia arriba, empezar desde el buscador
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({behavior: 'smooth'});
+    }
+    consultarAPI();
+  }, [busqueda, paginaactual]);
+
+  //definir la pagina anterior
+
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+    //para que la paginacion no vaya a numeros negativos
+    if(nuevaPaginaActual === 0) return;
+
+    guardarPaginaActual(nuevaPaginaActual);
+  }
+
+  const paginaSiguiente = () => {
+    
+    const nuevaPaginaActual = paginaactual + 1;
+
+    //para no pasar mas de la ultima pagina
+    if(nuevaPaginaActual > totalpaginas) return;
+
+    guardarPaginaActual(nuevaPaginaActual); 
+  }
+
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imagenes</p>
+
+        <Formulario
+          guardarBusqueda={guardarBusqueda}
+        />        
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes 
+          imagenes={imagenes}
+        />
+        {(paginaactual === 1) ? null : (   
+        <button 
+          type="button"
+          className="bbtn btn-info mr-1"
+          onClick={paginaAnterior}
+        >&laquo; Anterior</button>
+        )}
+        {(paginaactual === totalpaginas) ? null : (
+          <button 
+            type="button"
+            className="bbtn btn-info"
+            onClick={paginaSiguiente}
+        >Siguiente &raquo;</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
